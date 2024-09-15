9411fdeb5549693dd367cbc87c40d3acf1302ccf
ChanGyuLee <lay5137@naver.com>
2024-09-16 01:51:55 +0900

    기본 틀 구성

--- scripts.js
+++ scripts.js
@@ -1,8 +1,14 @@
-// 로그인 버튼 클릭 시 login.html로 이동
+// 로그인 및 회원가입 버튼 클릭 시 해당 페이지로 이동
 document.addEventListener('DOMContentLoaded', function() {
     const loginButton = document.getElementById('login-btn');
+    const signupButton = document.getElementById('signup-btn'); // getElementById로 변경
 
     loginButton.addEventListener('click', function() {
         window.location.href = 'login.html';
     });
+
+    signupButton.addEventListener('click', function() {
+        window.location.href = 'signup.html';
+    });
 });
+
